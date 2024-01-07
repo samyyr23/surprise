f093e0ac4aa95bd7f90469b27397c9a8fcb917e4
samyyr23 <155871484+samyyr23@users.noreply.github.com>
2024-01-07 17:12:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const letters = document.querySelectorAll(".letter");
+
+    letters.forEach((letter, index) => {
+        letter.style.animationDelay = `${index * 0.3}s`;
+    });
+});
 
