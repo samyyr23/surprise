2f9c3c780286bce5c2ff716d3c8d3da326445d61
samyyr23 <155871484+samyyr23@users.noreply.github.com>
2024-01-07 17:02:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let message = "EU TE AMO MUITO";
+    const letters = document.querySelectorAll(".letter");
 
-    for (let i = 0; i < message.length; i++) {
-        let span = document.createElement("span");
-        span.innerText = message[i];
-        span.style.left = i * 30 + "vw";
-        span.style.animationDelay = i * 0.5 + "s";
-        document.body.appendChild(span);
-    }
+    letters.forEach((letter, index) => {
+        letter.style.animationDelay = `${index * 0.3}s`;
+    });
 });
